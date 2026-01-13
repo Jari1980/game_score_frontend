ef672b4116dc621bb5f8baa75a20d5956fbbce95
Nadia <naaadz@gmail.com>
2026-01-12 21:37:58 -0500

    added darker theme

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,10 +2,10 @@
   <teleport to="body">
     <div
       v-if="open"
-      class="fixed inset-0 z-50 flex justify-center items-center bg-dark/80 backdrop-blur-xs"
+      class="fixed inset-0 z-50 flex justify-center items-center bg-darker/80 backdrop-blur-xs"
     >
       <div
-        class="bg-dark main-border p-12 relative w-[80%] md:w-[25rem] h-max max-h-screen overflow-auto"
+        class="bg-darker main-border p-12 relative w-[80%] md:w-[25rem] h-max max-h-screen overflow-auto"
       >
         <button
           class="absolute top-4 right-6 text-2xl cursor-pointer"
